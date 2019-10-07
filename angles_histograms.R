8caeb5e6e9398a28470d896655270908cc5591e8
Daniel Buchan <clone@myself.com>
2019-10-07 15:27:24 +0100

    tidied up R scripts to add eps output for paper submission

--- angles_histograms.R
+++ angles_histograms.R
@@ -12,12 +12,13 @@ transmembrane_slice <- angles_data[angles_data$group == "transmembrane",]
 ggplot(nuclear_slice, aes(x=radians)) + geom_histogram()
 mean(nuclear_slice$radians)
 
-ggplot(intracellular_slice, aes(x=radians)) + geom_histogram()
+ggplot(intracellular_slice, aes(x=radians)) + geom_histogram()+ xlab('Radians') + ylab("Count")+theme(text = element_text(size=20))
+ggsave("/scratch0/NOT_BACKED_UP/dbuchan/projects/interpro_word2vec/Fig7_intra-extra_radian_histogram.eps", width=10, height=7, dpi=300)
 mean(intracellular_slice$radians)
 
 ggplot(transmembrane_slice, aes(x=radians)) + geom_histogram()
 mean(transmembrane_slice$radians)
 
 duf_count_data <-  read.csv(file=paste(pathroot,"/dbuchan/Code/domain_word2vec/duf_counts.csv", sep=""), check.names=FALSE, strip.white = TRUE, sep=",",na.strings= c("NA", " ", ""))
-ggplot(duf_count_data, aes(x=Count)) + geom_histogram() + xlab('Number of GO terms assigned') + ylab("Frequency")
-ggsave("/scratch0/NOT_BACKED_UP/dbuchan/projects/interpro_word2vec/duf_assignment_frequencies.png", width=10, height=7, dpi=300)
+ggplot(duf_count_data, aes(x=Count)) + geom_histogram() + xlab('Number of GO terms assigned') + ylab("Frequency")+theme(text = element_text(size=20))
+ggsave("/scratch0/NOT_BACKED_UP/dbuchan/projects/interpro_word2vec/Fig9_duf_assignment_frequencies.eps", width=10, height=7, dpi=300)
